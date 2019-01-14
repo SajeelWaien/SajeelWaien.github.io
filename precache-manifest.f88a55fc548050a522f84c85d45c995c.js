62eb2926950d0e64896a087f024f173020573333
Sajeel Waien <sajeel_waien@live.com>
2019-01-14 19:27:25 +0500

    Updates

---
+++ precache-manifest.f88a55fc548050a522f84c85d45c995c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a76b51cffa30725fa0f9",
+    "url": "/static/js/main.a76b51cf.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a76b51cffa30725fa0f9",
+    "url": "/static/css/main.e6cf5dd1.chunk.css"
+  },
+  {
+    "revision": "d0346bf7c18a394869932b3e4b51d7bc",
+    "url": "/index.html"
+  }
+];
